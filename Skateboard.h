6d83a3f33ff479fab7919e2017e2ba8783f5e4f8
Parmers <51136819+Parmers@users.noreply.github.com>
2019-10-04 22:59:06 -0400

    Added Skateboard.h

---
+++ Skateboard.h
@@ -0,0 +1,23 @@
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include <ctime>
+#include "Vehicle.h"
+#include <stdlib.h>
+
+
+class Skateboard : public Vehicle {
+
+private:
+
+public:
+    Skateboard();
+
+    explicit Skateboard(string brand, string model);
+    virtual ~Skateboard();
+    virtual double mileageCalc(double time);
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_SKATEBOARD_H
